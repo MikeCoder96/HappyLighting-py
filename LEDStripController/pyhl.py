2fd3c722c6d086d98b99de809992d2c9fd22b546
MikeCoder96 <50410305+MikeCoder96@users.noreply.github.com>
2022-08-14 11:40:12 +0200

    Working mode and speed

--- LEDStripController/pyhl.py
+++ LEDStripController/pyhl.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 from functools import cached_property
 from bleak import BleakScanner, BleakClient
 from bleak.backends.device import BLEDevice
-from PyQt5.QtCore import QObject, pyqtSignal, QRect
+from PyQt5.QtCore import QObject, pyqtSignal, QRect, Qt
 
 from PyQt5.QtWidgets import (
     QApplication,
@@ -20,8 +20,9 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QWidget,
     QColorDialog,
+    QListWidget,
     QLabel,
-    QGraphicsColorizeEffect,
+    QSlider
 )
 
 DEBUG_LOGS = True
@@ -32,6 +33,12 @@ UART_TX_CHAR_UUID = ""
 UART_SAFE_SIZE = 20
 
 Colors = {"Red":0, "Green":0, "Blue":0}
+Modes = [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 
+         47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 
+         97, 98, 99]
+Speed = 0
+KodeUsed = -1
+isModeUsed = False
 
 def printLog(text):
     if DEBUG_LOGS:
@@ -76,6 +83,18 @@ class QBleakClient(QObject):
             except Exception as inst:
                 print(inst)
 
+    async def writeMode(self, idx):
+            global Speed
+            #new byte[] { 256 - 69, mode, (byte)(speed & 0xFF), 68 };
+            i_mode = Modes[idx]
+            lista = [256 - 69, i_mode, (Speed & 0xFF), 68]
+            values = bytearray(lista)
+            try:
+                printLog("Change Mode with ID {} ".format(i_mode))
+                await self.client.write_gatt_char(UART_TX_CHAR_UUID, values, False)
+            except Exception as inst:
+                print(inst)
+
     #TODO: Implement disconnect function
     def _handle_disconnect(self, device) -> None:
         printLog("Device was disconnected")
@@ -93,6 +112,43 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QtGui.QIcon('about_icon.png'))
         self.setWindowTitle("PyHL")
         
+        self.modeList = QListWidget(self)
+        
+        self.modeList.itemDoubleClicked.connect(self.selectMode)
+
+        self.horizontalSlider = QSlider(self)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setGeometry(QRect(160, 60, 160, 22))
+        self.horizontalSlider.setMinimum(1)
+        self.horizontalSlider.setMaximum(10)
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.horizontalSlider.valueChanged.connect(self.changeSpeed)
+
+        self.modeList.setGeometry(150, 90, 180, 180)
+        self.modeList.addItem("Pulsating rainbow")
+        self.modeList.addItem("Pulsating red")
+        self.modeList.addItem("Pulsating green")
+        self.modeList.addItem("Pulsating blue")
+        self.modeList.addItem("Pulsating yellow")
+        self.modeList.addItem("Pulsating cyan")
+        self.modeList.addItem("Pulsating purple")
+        self.modeList.addItem("Pulsating white")
+        self.modeList.addItem("Pulsating red/green")
+        self.modeList.addItem("Pulsating red/blue")
+        self.modeList.addItem("Pulsating green/blue")
+        self.modeList.addItem("Rainbow strobe")
+        self.modeList.addItem("Red strobe")
+        self.modeList.addItem("Green strobe")
+        self.modeList.addItem("Blue strobe")
+        self.modeList.addItem("Yellow strobe")
+        self.modeList.addItem("Cyan strobe")
+        self.modeList.addItem("Purple strobe")
+        self.modeList.addItem("white strobe")
+        self.modeList.addItem("Rainbow jumping change")
+        self.modeList.addItem("Pulsating RGB")
+        self.modeList.addItem("RGB jumping change")
+
+
         self.scan_button = QPushButton(self)
         self.scan_button.setText("Scan")
         self.scan_button.setGeometry(QRect(10, 10, 75, 23))
@@ -129,7 +185,14 @@ class MainWindow(QMainWindow):
         self.scan_button.clicked.connect(self.handle_scan)
         self.connect_button.clicked.connect(self.handle_connect)
         self.send_button.clicked.connect(self.handle_send)
+        self.modeList.itemDoubleClicked.connect(self.selectMode)
 
+
+    def selectMode(self, item):
+        global isModeUsed
+
+        isModeUsed = True
+        self.handle_mode(self.modeList.indexFromItem(item).row())
 
     @cached_property
     def devices(self):
@@ -170,6 +233,14 @@ class MainWindow(QMainWindow):
                 self.devices_combobox.insertItem(i, device.name, device)
         #self.log_edit.appendPlainText("Finish scanner")
 
+    def changeSpeed(self, value):
+        global Speed
+
+        Speed = value
+        if isModeUsed:
+            self.handle_mode(self.modeList.currentIndex().row())
+
+
     def handle_message_changed(self, message):
         pass
         #self.log_edit.appendPlainText(f"msg: {message.decode()}")
@@ -177,11 +248,18 @@ class MainWindow(QMainWindow):
     @qasync.asyncSlot()
     async def handle_send(self):
         global Colors
+        global isModeUsed
 
-        res = ColorSelector()
-        res.show()
+        isModeUsed = False
+        self.res = ColorSelector()
+        self.res.show()
         Colors = res.GetValue()
         await self.current_client.writeColor()
+
+
+    @qasync.asyncSlot()
+    async def handle_mode(self, idx):
+        await self.current_client.writeMode(idx)
 
 def main():
     app = QApplication(sys.argv)
